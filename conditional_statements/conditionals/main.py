0d17356031015a602283fb846e929da7d503ece0
michalba8 <>
2026-03-09 11:52:35 +0000

    Conditionals

--- conditional_statements/conditionals/main.py
+++ conditional_statements/conditionals/main.py
@@ -3,7 +3,7 @@ product_type = "Dairy"
 day_of_week = "Wednesday"
 if product_type == "Fruits" and day_of_week == "Monday":
     print("10% discount on Fruits today!")
-elif product_type == "Dairy" and day_of_week == "Tuesday":
+elif product_type == "Vegetables" and day_of_week == "Tuesday":
     print("15% discount on Vegetables today!")
 elif product_type == "Dairy" and day_of_week == "Wednesday":
     print("20% discount on Dairy today!")
